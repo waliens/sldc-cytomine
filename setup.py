7eb9808252031fd1550402b671edcbafb438258b
Waliens <romainmormont@hotmail.com>
2018-11-29 10:32:43 +0100

    update setup.py for pip

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = "1.1"
 
 setup(
-    name="cytomine-sldc",
-    version="1.0",
+    name="sldc-cytomine",
+    version=__version__,
     description="Cytomine-SLDC, a SLDC binding to Cytomine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["sldc_cytomine"],
+    url='https://github.com/waliens/sldc',
     author="Romain Mormont",
     author_email="romain.mormont@gmail.com",
-    packages=["cytomine_sldc"]
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=["sldc"]
 )
