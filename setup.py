0be04ea6f6c6299d2840d49be2e7f25fb8a29d57
Waliens <r.mormont@uliege.be>
2021-03-29 13:30:08 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 
 setup(
     name="sldc-cytomine",
