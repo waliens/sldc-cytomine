2f41100fe6c1d16167157defdb417069582b97b6
Romain Mormont <romain.mormont@cytomine.com>
2023-07-31 18:41:42 +0200

    chore: update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = "v2.2.0"
+__version__ = "v2.3.0"
 
 setup(
     name="sldc-cytomine",
