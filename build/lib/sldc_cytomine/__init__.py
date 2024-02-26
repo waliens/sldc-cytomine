1f346203004cb8cb5c67a012bd5417c5cec5e09a
Juliette Ponnet <juliette.ponnet@cytomine.com>
2024-02-26 17:52:48 +0100

    revert: remove build lib

--- build/lib/sldc_cytomine/__init__.py
+++
@@ -1,7 +0,0 @@
-from .image import CytomineSlide
-from .tile import CytomineTile, CytomineIIPTile
-from .tile_builder import TileCache, CytomineTileBuilder
-
-__all__ = [
-    "CytomineSlide", "CytomineTile", "TileCache", "CytomineTileBuilder", "CytomineIIPTile"
-]
